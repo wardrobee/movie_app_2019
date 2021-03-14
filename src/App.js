5162914694f8e3d098af77d88507f25f85d4fb06
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-03-14 14:17:03 +0900

    Dynamic data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-function Food(props) {
-  console.log(props);
-  return <h3>I love potato</h3>;
+function Food({ fav }) {
+  return (
+    <h3>
+      I like {fav}
+    </h3>
+  );
 }
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Food fav="kimchi" something={true} papap={[1, 2, 3, true]} />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="pancake" />
+      <Food fav="juice" />
+
       {/* fav라는 property w value kimchi */}
     </div>
   );
