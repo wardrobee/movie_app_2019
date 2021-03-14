337ffbc90ef0407a1a983d0b4be7bd0d50ddb4ae
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-03-15 03:40:37 +0900

    Loading for 5 sec

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
+// Jinny Choi
+// Movie App 2019
+// Nomad Coders ReactJS로 영화 웹 서비스 만들기
+
 import React from "react";
 
 // class component
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor called");
-  }
-
-  // state object, put data of ur component inside state, data that will change
   state = {
-    count: 0
-  };
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true
   };
 
   componentDidMount() {
-    console.log("componentDidMount called");
+    // Js thing not React thing
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 5000);
   }
 
-  componentDidUpdate() {
-    console.log("componentDidUpdate called");
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount called");
-  }
   render() {
-    console.log("render called");
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>
-          The number is: {this.state.count}
-        </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
     );
   }
