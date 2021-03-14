fda5cff3549358363fb6e6ada0edefc3f7e6ac27
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-03-14 15:09:13 +0900

    Map each dish from foodILike list of objs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
-function Food({ fav }) {
+function Food({ name, picture }) {
   return (
-    <h3>
-      I like {fav}
-    </h3>
+    <div>
+      <h2>
+        I like {name}
+      </h2>
+      <img src={picture} />
+    </div>
   );
 }
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://www.wearesovegan.com/wp-content/uploads/2020/09/howtomakevegankimchirecipe-h3.jpg"
+  },
+  {
+    name: "Chicken",
+    image:
+      "https://cdn.cms.prod.nypr.digital/images/8f417ad16chicken_main-jpg.2e16d0ba.fill-661x496.jpg"
+  },
+  {
+    name: "Ramen",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Shoyu_Ramen.jpg/1200px-Shoyu_Ramen.jpg"
+  },
+  {
+    name: "Chips",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81vJyb43URL._SL1500_.jpg"
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="pancake" />
-      <Food fav="juice" />
-
-      {/* fav라는 property w value kimchi */}
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
     </div>
   );
 }
