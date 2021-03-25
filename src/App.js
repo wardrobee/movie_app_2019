ffbc00cef428e0a98b2c71bd258b61279ebe2847
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-03-26 03:49:57 +0900

    Install React Router Dom & Organize directories

--- src/App.js
+++ src/App.js
@@ -3,57 +3,9 @@
 // Nomad Coders ReactJS로 영화 웹 서비스 만들기
 
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
 
-// class component
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-
-  // async bc function might take time, so might need to wait
-  // can't use await without async
-  getMovies = async () => {
-    const { data: { data: { movies } } } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    // es6 Destructuring_assignment
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading
-          ? <div className="loader">
-              <span className="loader__text">Loading...</span>
-            </div>
-          : <div className="movies">
-              {movies.map(movie => {
-                return (
-                  <Movie
-                    key={movie.id}
-                    id={movie.id}
-                    year={movie.year}
-                    title={movie.title}
-                    summary={movie.summary}
-                    poster={movie.medium_cover_image}
-                    genres={movie.genres}
-                  />
-                );
-              })}
-            </div>}
-      </section>
-    );
-  }
+function App() {
+  return <span>something</span>;
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,59 @@
+// Jinny Choi
+// Movie App 2019
+// Nomad Coders ReactJS로 영화 웹 서비스 만들기
+
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+// class component
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  // async bc function might take time, so might need to wait
+  // can't use await without async
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    // es6 Destructuring_assignment
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading
+          ? <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div>
+          : <div className="movies">
+              {movies.map(movie => {
+                return (
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                  />
+                );
+              })}
+            </div>}
+      </section>
+    );
+  }
+}
+
+export default Home;
