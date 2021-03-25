9f90814dd67be2440be8798508bfdedf37227629
Jinny Choi <jchoi@Jinnys-MacBook-Air.local>
2021-03-26 04:06:51 +0900

    Create Route Components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@
 // Nomad Coders ReactJS로 영화 웹 서비스 만들기
 
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
 function App() {
-  return <span>something</span>;
+  return (
+    <HashRouter>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      {/* <Route path="/home">
+        <h1>Home</h1>
+      </Route>
+      <Route path="/home/introduction">
+        <h1>Introduction</h1>
+      </Route>
+      <Route path="/about">
+        <h1>About</h1>
+      </Route> */}
+    </HashRouter>
+  );
 }
 
 export default App;
